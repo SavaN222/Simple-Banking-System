c7d2913c063b8f4eae938ae369825735df943274
SavaN22 <savanakarada22@gmail.com>
2020-12-18 13:50:06 +0100

    Bank class with menu options

---
+++ com/sava/Bank.java
@@ -0,0 +1,13 @@
+package com.sava;
+
+public class Bank {
+
+    /**
+     * Print's available options in bank
+     */
+    public void menu() {
+        System.out.println("1. Create an account");
+        System.out.println("2. Log into account");
+        System.out.println("0. Exit");
+    }
+}
