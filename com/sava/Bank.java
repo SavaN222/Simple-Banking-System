ec8e686c7b09cb2c0ab037d4fa24f72e3dbddcfc
SavaN22 <savanakarada22@gmail.com>
2020-12-18 14:16:50 +0100

    Generate random number by length and concate them

--- com/sava/Bank.java
+++ com/sava/Bank.java
@@ -10,4 +10,36 @@ public class Bank {
         System.out.println("2. Log into account");
         System.out.println("0. Exit");
     }
+
+    public Customer createAccount() {
+
+    }
+
+    /**
+     * Generate random number
+     * @param length - digits in random number.
+     * @return int[]
+     */
+   private int[] generateRandomNumber(int length) {
+        int[] nums = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            nums[i] = (int) (Math.random() * 10) - 1;
+        }
+        return nums;
+   }
+
+    /**
+     * Concatenate array of numbers in one number.
+     * @param digits - array of numbers to use.
+     * @return String
+     */
+   private String concatenateDigits(int[] digits) {
+        StringBuilder sb = new StringBuilder(digits.length);
+
+        for (int digit : digits) {
+            sb.append(digit);
+        }
+        return sb.toString();
+   }
 }
--- com/sava/Main.java
+++ com/sava/Main.java
@@ -1,9 +1,29 @@
 package com.sava;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean flag = true;
+        Bank bank = new Bank();
+        Scanner sc = new Scanner(System.in);
+        while (flag) {
+            bank.menu();
+            int option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    // createAcoount //
+                    break;
+                case 2:
+                    //login//
+                    break;
+                case 0:
+                    flag = false;
+                    break;
+            }
+        }
     }
 
 }
