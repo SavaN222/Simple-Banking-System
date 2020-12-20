2c51439aaad6430375a6a7105f28385c5c3bdff5
SavaN22 <savanakarada22@gmail.com>
2020-12-20 22:00:56 +0100

    Create table

--- com/sava/Database.java
+++ com/sava/Database.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Database {
-    private final static String PATH = "jdbc:sqlite:";
+//    private final static String PATH = "jdbc:sqlite:";
+    private final static String PATH = "jdbc:sqlite:C:\\Users\\Korisnik\\IdeaProjects\\Simple Banking System\\";
     private Connection conn;
     private Statement stmt;
     private final static String TABLE = "cards";
@@ -19,9 +20,21 @@ public class Database {
         try {
             Connection conn = DriverManager.getConnection(PATH + dbName);
             stmt = conn.createStatement();
+            stmt.execute("DROP TABLE IF EXISTS " + TABLE);
+
+            stmt.execute("CREATE TABLE IF NOT EXISTS " + TABLE + "(" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    COLUMN_NUMBER + " TEXT," +
+                    COLUMN_PIN + " TEXT," +
+                    COLUMN_BALANCE + " INTEGER DEFAULT 0)");
+
         } catch (SQLException e) {
             System.out.println("DATABASE ERROR: " + e.getMessage());
         }
+    }
+
+    public void createAccount(String cardNumber, String pinCode) {
+
     }
 
 }
--- com/sava/Main.java
+++ com/sava/Main.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        if (args[0].equals("-fileName") && args[1] != null) {
+            Database db = new Database(args[1]);
+        } else {
+            System.out.println("Database name is not supplied. \nExiting the program.");
+        }
+
         boolean flag = true;
         Bank bank = new Bank();
         Scanner sc = new Scanner(System.in);
